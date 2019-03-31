7045c66f6b7478dfa8e9c8f9cf42b412cab63d13
Kelli Landry <kelli.landry@waitrapp.com>
2019-03-31 18:51:56 -0500

    implementing loading reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combinedReducers } from 'redux'
 import authedUser from './authedUser'
 import users from './user'
 import polls from './polls'
+import { loadingBarReducer } from 'react-redux-loading'
 
 export default combinedReducers({
     authedUser,
     users,
     polls,
+    loadingBar: loadingBarReducer,
 })
